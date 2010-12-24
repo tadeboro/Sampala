38833849609a84832e1155906380fd4b6fbf18de
Tadej Borovšak <tadeboro@gmail.com>
2010-12-24 14:28:11 +0100

    Fix small typo in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 test -n "$srcdir" || srcdir=$(dirname "$0")
-test -n "$srcdir" || srcdit=.
+test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
   AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv
